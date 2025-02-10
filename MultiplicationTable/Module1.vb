faf75b1eab51a88b3d468105b941df1a9df9b8d7
Christopher-isu <christopherzakrev@isu.edu>
2025-02-09 20:00:27 -0700

    Add project files.

---
+++ MultiplicationTable/Module1.vb
@@ -0,0 +1,23 @@
+﻿'ChristopherZ
+'Spring 2025
+'RCET2265
+'Operators Example
+'https://github.com/Christopher-isu/SayMyName.git
+
+Option Explicit On 'forces all variables to be declared
+Option Strict On 'forces all variables to be declared and data types to match
+
+Module Module1
+
+    Sub Main()
+        Dim i As Integer
+        For i = 1 To 10
+            For j = 1 To 10
+                Console.Write(CStr(i * j).PadLeft(3) & " |")
+            Next
+            Console.WriteLine(i)
+        Next
+
+    End Sub
+
+End Module
